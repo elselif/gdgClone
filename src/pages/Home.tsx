ce752c0c971e00f6609be315dadefb9ee905e80f
Elif <78614794+elselif@users.noreply.github.com>
2022-08-20 01:29:27 +0300

    bugİmage

---
+++ src/pages/Home.tsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import homeHi from "../img/homeHi";
+
+export default function Home() {
+  return (
+    <div>
+        <h1>deneme</h1>
+        <img src={homeHi} ></img>
+ </div>
+  )
+}
