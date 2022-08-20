81ad2112989589104b82efa83ba4bfc64a2d8c3c
Elif <78614794+elselif@users.noreply.github.com>
2022-08-20 15:51:49 +0300

    -

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,10 +1,17 @@
 import React from 'react'
+import Box from '@mui/material/Box';
+import Navbar from '../components/Navbar';
+
 
 export default function Home() {
   return (
     <>
-      <div className='bg-red-500 h-[312px] w-[100%]'>
-          <img src={require("../images/hello.png")} alt='i' width="380px" />
+      <div >
+      <Navbar/>
+        <Box className="items-start justify-items-start  flex flex-row  " sx={{ }}>
+          <img src={require("../images/hello.png")} alt='i' width="500px " height="500px" className='' />
+          <h1 className='text-4xl	pt-40 mobil:text-7xl  '>Make good things <span className='text-blue-600	'>together</span>.</h1>
+          </Box>
       </div>
     </>
   )
