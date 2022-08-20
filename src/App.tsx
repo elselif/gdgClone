6ec237514b2513ac1692917f4ff6c4ec9eb7d6c1
Cekap Yakup <61888196+cekapykp@users.noreply.github.com>
2022-08-20 03:07:36 +0300

    responsive-work

--- src/App.tsx
+++ src/App.tsx
@@ -1,22 +1,17 @@
 import React from 'react';
 
 import './App.css';
-import FixedBottomNavigation from './components/BottomApBar';
+import MobilMenu from './components/MobilMenu';
 import Navbar from './components/Navbar';
+import Home from './pages/Home';
 
 
 function App() {
   return (
     <div className="App">
-
       <Navbar />
-      <FixedBottomNavigation />
-
-      <Navbar />   
-    
-   <FixedBottomNavigation/>
-
-
+      <Home />
+      <MobilMenu />
     </div>
   );
 }
--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -3,8 +3,15 @@ import React from 'react'
 export default function Home() {
   return (
     <>
-      <div className='bg-red-500 h-[312px] w-[100%]'>
-          <img src={require("../images/hello.png")} alt='i' width="380px" />
+      <div className=' bg-green-200 w-full '>
+        <div className='bg-red-500 flex mx-auto h-[312px] w-[1200px]'>
+          <div className=' w-[500px]'>
+            <img src={require("../images/hello.png")} alt='i' className='mx-auto h-auto w-[340px] overflow-hidden' />
+          </div>
+          <div className='w-full'>
+            <div className='text-4xl'>Make good things together.</div>
+          </div>
+        </div>
       </div>
     </>
   )
