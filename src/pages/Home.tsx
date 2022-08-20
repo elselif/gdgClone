c0935181964e29b9a38a21fc6e638272263a298f
Cekap Yakup <61888196+cekapykp@users.noreply.github.com>
2022-08-20 16:22:07 +0300

    Update Home.tsx

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -1,14 +1,15 @@
 import React from 'react'
 
+
 export default function Home() {
   return (
     <>
-      <div className=' bg-green-200 w-full '>
-        <div className='bg-red-500 flex mx-auto h-[312px] w-[1200px]'>
-          <div className=' w-[500px]'>
-            <img src={require("../images/hello.png")} alt='i' className='mx-auto h-auto w-[340px] overflow-hidden' />
+      <div className=' m-3 bg-green-200'>
+        <div className='bg-red-500 flex mx-auto h-[280px] max-w-[1200]'>
+          <div className=' w-[280px] bg-images'>
+            <img src={require("../images/hello.png")} alt='i' className='w-full mx-auto h-auto w-[auto] sm:w-block overflow-hidden' />
           </div>
-          <div className='w-full'>
+          <div className='w-auto'>
             <div className='text-4xl'>Make good things together.</div>
           </div>
         </div>
