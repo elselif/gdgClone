8aa7caab9102cb7d8eebf977d2955a0032bd08cf
Cekap Yakup <61888196+cekapykp@users.noreply.github.com>
2022-08-21 00:08:45 +0300

    fixed-brach

--- src/pages/Home.tsx
+++ src/pages/Home.tsx
@@ -7,48 +7,43 @@ import ButtonBase from '@mui/material/ButtonBase';
 import Navbar from '../components/Navbar';
 import { start } from 'repl';
 
-const Img = styled('img')({
-  margin: 'auto',
-  display: 'block',
-  maxWidth: '100%',
-  maxHeight: '100%',
-});
+
 
 export default function ComplexGrid() {
   return (
     <>
       <Navbar />
-      <Paper
-        sx={{
-          p: 2,
-          margin: 'auto',
-          maxWidth: 1200,
-          flexGrow: 1,
-        }}
-      >
-        <Grid container spacing={2}>
-          <Grid item>
-            <ButtonBase sx={{ width: 290, height: 280 }}>
-              <img src={require("../images/hello.png")} alt='i' className='w-auto h-auto' />
-            </ButtonBase>
-          </Grid>
-          <Grid item xs={12} sm container>
-            <Grid item xs container direction="column" spacing={2}>
-              <Grid item xs>
-                <div className='flex float-left'>
-                  Selam
-                </div>
-              </Grid>
-              <Grid item>
-                <Typography sx={{ cursor: 'pointer' }} variant="body2">
-                  Remove
-                </Typography>
-              </Grid>
+
+
+      <Grid container spacing={2}>
+        <Grid item>
+          <ButtonBase className='w-auto h-auto lg:w-[500px] '>
+            <img src={require("../images/hello.png")} alt='i' className='w-auto h-auto' />
+          </ButtonBase>
+        </Grid>
+        <Grid item xs={12} sm container>
+          <Grid item xs container spacing={2}>
+            <Grid className=' ' item xs >
+              <Typography className='' variant="h3" component="div" sx={{}}>
+                Make good things <span className='text-blue-800'>together</span>.
+              </Typography>
+              <Typography variant="body2" gutterBottom>
+                GDG Istanbul
+              </Typography>
+              <Typography variant="body2" color="text.secondary">
+                GDG Istanbul, Google teknolojilerine ilgi duyan, bir şeyler geliştirmek ve bilgi paylaşımında bulunmak isteyen insanlardan oluşan bir topluluktur.
+              </Typography>
+            </Grid>
+            <Grid item>
+              <Typography sx={{ cursor: 'pointer' }} variant="body2">
+                Remove
+              </Typography>
             </Grid>
 
           </Grid>
         </Grid>
-      </Paper>
+      </Grid>
+
     </>
   );
 }
