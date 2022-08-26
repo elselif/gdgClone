90218660b9592ae16b05fa3b0166e315e83b84d1
Cekap Yakup <61888196+cekapykp@users.noreply.github.com>
2022-08-26 20:01:48 +0300

    fix-path

--- src/pages/HomePage.tsx
+++ src/pages/HomePage.tsx
@@ -1,9 +1,9 @@
-import { HelloDiv } from '../Layouts/HelloDiv';
 import { About } from '../Layouts/About';
 import { Meetings } from '../Layouts/Meeting';
-import {  EventsPage } from '../Layouts/EventPage';
+import { EventsPage } from '../Layouts/EventPage';
 import { Partner } from '../Layouts/Partners';
 import WhatWeDo from '../Layouts/WhatWeDo';
+import { HelloDiv } from '../Layouts/HelloDiv';
 
 export default function HomePage() {
   return (
