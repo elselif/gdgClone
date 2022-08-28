f22fed04ea1b7fc48d51d78bb6506a0dc3b1dc60
Cekap Yakup <61888196+cekapykp@users.noreply.github.com>
2022-08-28 15:10:50 +0300

    half-update

--- src/components/ListItemButton.tsx
+++ src/components/ListItemButton.tsx
@@ -1,7 +1,20 @@
+import { Button, Grid } from '@mui/material'
 import React from 'react'
 
 export const ListItemButton = () => {
   return (
-    <div>ListItemButton</div>
+    <div>
+      <Grid>
+        <Grid.Row>
+          <Grid.Column width={16}>
+            <Button>
+              <ListItemIcon><HomeIcon /></ListItemIcon>
+              <ListItemText>Home</ListItemText>
+            </Button>
+          </Grid.Column>
+        </Grid.Row>
+        
+      </Grid>
+    </div>
   )
 }
